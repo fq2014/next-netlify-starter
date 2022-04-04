39bed6a6f0de179ed47ba70737bd9a68e0f3b951
fq2014 <78662982+fq2014@users.noreply.github.com>
2022-04-04 00:42:59 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,15 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Updates Coming Soon!
+          <form action="addstudent.php">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname"><br>
+            <label for="email">Email:</label><br>
+            <input type="email" id="email" name="email"><br>
+            <input type="submit" value="Submit">
+          </form>
         </p>
       </main>
 
